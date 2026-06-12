1a5ba3382b81083db7caeff72025035a04ada297
Nurbu <lamanurbu2@gmail.com>
2026-06-12 14:20:36 -0400

    Created the database and added the details to resources file to connect the newly created database concerttracker.

--- src/test/java/com/pluralsight/concerttracker/ConcertTrackerApplicationTests.java
+++ src/test/java/com/pluralsight/concerttracker/ConcertTrackerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ConcertTrackerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
