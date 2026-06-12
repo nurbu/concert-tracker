a24915a9cacf664803f351f31bee5bd9f6909222
Nurbu <lamanurbu2@gmail.com>
2026-06-12 13:32:04 -0400

    Initial commit

---
+++ src/main/java/com/pluralsight/concerttracker/ConcertTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.concerttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertTrackerApplication.class, args);
+	}
+
+}
