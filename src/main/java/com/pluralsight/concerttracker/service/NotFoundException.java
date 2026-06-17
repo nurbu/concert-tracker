ac02c77b08915741aa30017b6cb3c3ce213e0b09
Nurbu <lamanurbu2@gmail.com>
2026-06-17 16:40:10 -0400

    Fixed NotFoundException so it properly fit project and tell when a specific model is missing something.

--- src/main/java/com/pluralsight/concerttracker/service/NotFoundException.java
+++ src/main/java/com/pluralsight/concerttracker/service/NotFoundException.java
@@ -2,7 +2,7 @@ package com.pluralsight.concerttracker.service;
 
 public class NotFoundException extends RuntimeException {
 
-    public NotFoundException(long id) {
-        super("No sneaker found with id " + id);
+    public NotFoundException(String model, long id) {
+        super("No " + model + " found with id: " + id);
     }
 }
